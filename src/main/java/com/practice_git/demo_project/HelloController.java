2ea38fb07328793b14c4a2da8c80305a7b1b3eb7
testcaseleetcode <testcaseleetcode@gmail.com>
2026-04-08 22:47:06 +0530

    Added ResponseBody to Methods

--- src/main/java/com/practice_git/demo_project/HelloController.java
+++ src/main/java/com/practice_git/demo_project/HelloController.java
@@ -2,10 +2,12 @@ package com.practice_git.demo_project;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
     @GetMapping("/")
+    @ResponseBody
     public String sayHello() {
         return "Hello";
     }
--- src/main/java/com/practice_git/demo_project/LoginController.java
+++ src/main/java/com/practice_git/demo_project/LoginController.java
@@ -1,9 +1,13 @@
 package com.practice_git.demo_project;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 public class LoginController {
     @GetMapping("/login")
+    @ResponseBody
     public String getLoginPage() {
         return "Login Page";
     }
